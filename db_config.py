41b6b9460abd5e38b740b4cb9d34dbd143a982b4
sunnynew <mail2connect@gmail.com>
2019-07-28 15:00:53 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -5,7 +5,7 @@ mysql = MySQL()
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'db_emp'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
