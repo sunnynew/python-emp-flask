2a0528a3cdf55188ec3c08bbf4c94df30a976b40
sunnynew <mail2connect@gmail.com>
2019-07-28 17:09:53 +0530

    first commit

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
-	if auth and auth.username == 'username' and auth.password == 'password':
-	    return f(*args, **kwargs)
+        if auth and auth.username == 'username' and auth.password == 'password':
+           return f(*args, **kwargs)
 
-	return make_response('Could not verify your login!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        return make_response('Could not verify your login!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     return decorated 
 	
 @app.route('/add', methods=['POST'])
@@ -85,7 +85,7 @@ def emp(id):
 @app.route('/update', methods=['PUT'])
 def update_emp():
 	try:
-		_json = request.json
+                _json = request.json
                 _userId = _json['userId']
                 _jobTitleName = _json['jobTitleName']
                 _firstName = _json['firstName']
@@ -96,19 +96,19 @@ def update_emp():
                 _phoneNumber = _json['phoneNumber']
                 _emailAddress = _json['emailAddress']
 		# validate the received values
-		if _userId and _jobTitleName and _firstName and _emailAddress and _employeeCode and request.method == 'PUT':
+                if _userId and _jobTitleName and _firstName and _emailAddress and _employeeCode and request.method == 'PUT':
 			#save updates
-			sql = "UPDATE tbl_emp SET userId=%s, jobTitleName=%s, firstName=%s, lastName=%s, preferredFullName=%s, region=%s, phoneNumber=%s, emailAddress=%s WHERE employeeCode=%s"
-			data = (_userId, _jobTitleName, _firstName, _lastName, _preferredFullName, _region, _phoneNumber, _emailAddress, _employeeCode,)
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			cursor.execute(sql, data)
-			conn.commit()
-			resp = jsonify('Employee record updated successfully!')
-			resp.status_code = 200
-			return resp
-		else:
-			return not_found()
+                        sql = "UPDATE tbl_emp SET userId=%s, jobTitleName=%s, firstName=%s, lastName=%s, preferredFullName=%s, region=%s, phoneNumber=%s, emailAddress=%s WHERE employeeCode=%s"
+                        data = (_userId, _jobTitleName, _firstName, _lastName, _preferredFullName, _region, _phoneNumber, _emailAddress, _employeeCode,)
+                        conn = mysql.connect()
+                        cursor = conn.cursor()
+                        cursor.execute(sql, data)
+                        conn.commit()
+                        resp = jsonify('Employee record updated successfully!')
+                        resp.status_code = 200
+                        return resp
+                else:
+                        return not_found()
 	except Exception as e:
 		print(e)
 	finally:
@@ -144,4 +144,4 @@ def not_found(error=None):
 		
 if __name__ == "__main__":
 #    app.run()
-     app.run(host='0.0.0.0', port=80, debug=True)
+     app.run(host='0.0.0.0', port=5000, debug=True)
