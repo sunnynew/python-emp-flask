a1051f86da4f089252923e1fe3a7a1641af5a963
sunnynew <mail2connect@gmail.com>
2019-07-29 17:40:22 +0530

    first commit

--- db_config.py
+++ db_config.py
@@ -7,5 +7,6 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'db_emp'
-app.config['MYSQL_DATABASE_HOST'] = '10.2.1.202'
+app.config['MYSQL_DATABASE_HOST'] = '10.2.1.27'
+#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
