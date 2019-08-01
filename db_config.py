1233a484cbf7be740b83d056a002811ab5366de1
sunnynew <mail2connect@gmail.com>
2019-08-01 12:40:54 +0530

    first commit

--- db_config.py
+++ db_config.py
@@ -1,6 +1,6 @@
 from app import app
 from flaskext.mysql import MySQL
-
+import os
 mysql = MySQL()
  
 # MySQL configurations
@@ -9,7 +9,8 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'db_emp'
 
 #We are making /etc/hosts file entry on webservers for MySQL server through Ansible playbook. Please check app_playbook.yml for more details. Better to have a route53 inernal DNS configured.
-app.config['MYSQL_DATABASE_HOST'] = 'mysql_server'
+#app.config['MYSQL_DATABASE_HOST'] = 'mysql_server'
+app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
 
 #For localhost testing.
 #app.config['MYSQL_DATABASE_HOST'] = 'localhost'
